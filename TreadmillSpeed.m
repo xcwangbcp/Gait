e34309abcadd07c19712eb33ed59222ec989665f
xcwangbcp <45841039+xcwangbcp@users.noreply.github.com>
2021-12-24 16:11:08 +0800

    centered the title in figure 1 and 2

--- TreadmillSpeed.m
+++ TreadmillSpeed.m
@@ -1,22 +1,20 @@
-function [Vt] = TreadmillSpeed(raw,strat_line,end_line,Fs,filename)
-% right_foot_x   = raw(strat_line:end_line,46);
-right_foot_x   = raw(strat_line:end_line,26);
-[tx,X]         = smooth_not_detrend(right_foot_x ,Fs);
+function [Vt]  = TreadmillSpeed(raw,strat_line,end_line,M)
+
+right_foot_x   = raw(strat_line:end_line,M.rfoot_colum(1));
+[tx,X]         = smooth_not_detrend(right_foot_x ,M.Fs);
 TD             = [tx;X.'];
-%disp(TD)
 
 figure
-title(['Monkey', filename, ' treadmill speed base on right foot X-dimension move'])
 
 subplot(2,2,1)
 plot(tx,X,'b');
-title(['Monkey', filename, ' right foot X-dimension'])
+title(' right foot X-dimension')
 xlabel('t(s)')
 ylabel('X(pixle)')
 
 subplot(2,2,2)
 plot(tx,X,'b');
-title(['Monkey', filename, ' peaks and troughs'])
+title(' peaks and troughs')
 xlabel('t(s)')
 ylabel('X(pixle)')
 hold on
@@ -34,7 +32,7 @@ hold off
 
 subplot(2,2,3)
 plot(tx,X,'b');
-title(['Monkey', filename, ' filtered peaks pairs'])
+title(' filtered peaks pairs')
 xlabel('t(s)')
 ylabel('X(pixle)')
 hold on
@@ -56,8 +54,8 @@ for i = 1:times
     if txMin(locstxMin(i)) - txMax(i) > 0.4 %time filter here, must more than 0.3(s)
         line([txMax(i),txMin(locstxMin(i))],[XMax(i),XMin(locstxMin(i))],'color','r')
         hold on
-        t_to_regress = [TD(1,locsXMax(i) + 0.1*Fs:locsXMin(find(locsXMin > locsXMax(i),1))-0.1*Fs)];
-        X_to_regress = [TD(2,locsXMax(i) + 0.1*Fs:locsXMin(find(locsXMin > locsXMax(i),1))-0.1*Fs)];
+        t_to_regress = [TD(1,locsXMax(i) + 0.1*M.Fs:locsXMin(find(locsXMin > locsXMax(i),1))-0.1*M.Fs)];
+        X_to_regress = [TD(2,locsXMax(i) + 0.1*M.Fs:locsXMin(find(locsXMin > locsXMax(i),1))-0.1*M.Fs)];
         [b,bint,r,rint,stats] = regress(X_to_regress.',[ones(size(t_to_regress')),t_to_regress']);
     end
 end
@@ -65,7 +63,7 @@ hold off
 
 subplot(2,2,4)
 plot(tx,X,'b');
-title(['Monkey', filename, ' linear regression line'])
+title(' linear regression line')
 xlabel('t(s)')
 ylabel('X(pixle)')
 hold on
@@ -85,8 +83,8 @@ times = min(length(locstxMin),length(txMax));
 coef = [];
 for i = 1:times
     if txMin(locstxMin(i)) - txMax(i) > 0.4 %time filter here, must more than 0.3(s)
-        t_to_regress = [TD(1,locsXMax(i) + 0.1*Fs:locsXMin(find(locsXMin > locsXMax(i),1))-0.1*Fs)];
-        X_to_regress = [TD(2,locsXMax(i) + 0.1*Fs:locsXMin(find(locsXMin > locsXMax(i),1))-0.1*Fs)];
+        t_to_regress = [TD(1,locsXMax(i) + 0.1*M.Fs:locsXMin(find(locsXMin > locsXMax(i),1))-0.1*M.Fs)];
+        X_to_regress = [TD(2,locsXMax(i) + 0.1*M.Fs:locsXMin(find(locsXMin > locsXMax(i),1))-0.1*M.Fs)];
         [b,bint,r,rint,stats] = regress(X_to_regress.',[ones(size(t_to_regress')),t_to_regress']);
         if b(2) < -50 && stats(1) > 0.98 %speed filter and r^2 filter here
             z = b(1)+b(2)*t_to_regress;
